f17830fd45338854cf0491dc79faf2cce6547375
Robin Xuan <xuanzhaopeng@gmail.com>
2019-04-06 16:50:59 +0200

    Rename project Spec.Web.Android to Spec.Web.Mobile

--- Spec.Web.Mobile/Features/SimpleTest.feature.cs
+++ Spec.Web.Mobile/Features/SimpleTest.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Spec.Web.Android.Features
+namespace Spec.Web.Mobile.Features
 {
     using TechTalk.SpecFlow;
     
